ecc99c3ea6eb7fc397e76d854ee9de010bcf3e02
Rohan Menezes <menezesr08@gmail.com>
2022-08-25 12:40:43 +0100

    Create exchange contract

---
+++ test/Exchange.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Exchange", () => {
+  let deployer, feeAccount, exchange;
+  const feePercent = 10;
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    feeAccount = accounts[1];
+
+    const Exchange = await ethers.getContractFactory("Exchange");
+    exchange = await Exchange.deploy(feeAccount.address, feePercent);
+  });
+
+  describe("Deployment", () => {
+    it("tracks the fee account", async () => {
+      expect(await exchange.feeAccount()).to.equal(feeAccount.address);
+    });
+
+    it("tracks the fee percet", async () => {
+        expect(await exchange.feePercent()).to.equal(feePercent);
+      });
+  });
+});
