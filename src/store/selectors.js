f23ef885f945a799081da7d7292168e70a4349ce
Rohan Menezes <menezesr08@gmail.com>
2022-09-15 15:49:38 +0100

    my orders done

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -98,6 +98,66 @@ const decorateOrder = (order, tokens) => {
   };
 };
 
+export const myFilledOrdersSelector = createSelector(
+  account,
+  tokens,
+  filledOrders,
+  (account, tokens, orders) => {
+    if (!tokens[0] || !tokens[1]) {
+      return;
+    }
+
+   
+
+    orders = orders.filter((o) => o.user === account || o.creator === account)
+   
+    orders = orders.filter(
+      (o) =>
+        o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address
+    );
+    orders = orders.filter(
+      (o) =>
+        o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address
+    );
+
+    orders = orders.sort((a, b) => b.timestamp - a.timestamp);
+  
+    orders = decorateMyFilledOrders(orders, account, tokens)
+ 
+    return orders;
+  }
+);
+
+const decorateMyFilledOrders = (orders, account, tokens) => {
+  return (
+    orders.map((order) => {
+      order = decorateOrder(order, tokens)
+      order = decorateMyFilledOrder(order, account, tokens)
+      return order;
+    })
+  )
+
+};
+
+const decorateMyFilledOrder = (order, account, tokens) => {
+ const myOrder = order.creator === account;
+ let orderType 
+ if(myOrder) {
+  orderType = order.tokenGive === tokens[1].address ? 'buy' : 'sell'
+ } else {
+  orderType = order.tokenGive === tokens[1].address ? 'sell' : 'buy'
+ }
+
+  return ({
+    ...order,
+    orderType,
+    orderClass: (orderType === 'buy' ? GREEN : RED),
+    orderSign: (orderType === 'buy' ? '+' : '-'),
+  });
+};
+
+
+
 export const filledOrdersSelector = createSelector(
   filledOrders,
   tokens,
